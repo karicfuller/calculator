3b01954ee547effbc998bf9ad7974ba13bda5422
Kari Fuller <kari.c.fuller@gmail.com>
2018-05-20 10:24:20 -0700

    add on feature to add age and height and provide total

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,25 @@ var sayHi = function() {alert('Hello Jay'); };
 
 sayHi()
 
-
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+
+var askSize = function() {alert('What is your age plus your weight?'); };
+
+askSize()
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt('Enter your age'));
+var number2 = parseInt(prompt('Enter your height'));
 var result = add(number1, number2);
 alert(result);
